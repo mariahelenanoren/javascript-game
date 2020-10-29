c352e9a070be5535261e0a2a7c2dfc4b54a6c1c0
mariahelenanoren <marialinder97@hotmail.com>
2020-10-29 13:31:14 +0100

    fixed to appearance and disappearance of input fields

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ const wrapper = document.querySelector(".wrapper");
 const footer = document.querySelector(".footer");
 const subButton = document.querySelector(".submit");
 
-const inputMessage = document.querySelector(".text-input-container");
+const messageInput = document.createElement("input");
+const nameInput = document.createElement("input");
+const element = document.querySelector(".user-inputs");
+
+let nameWritteninGuestbook = false;
 
 menuButtons = [
     windowBtn = document.querySelector("#window"),
@@ -22,6 +26,8 @@ menuButtons = [
 
 let fireIsBurning = false;
 let messageWrittenOnWindow = false;
+let keysObtained = [] //push the keys, if right key is obtained door can be opened
+let penObtained = false;
 
 let matchCounter = 2;
 currentScene = 0;
@@ -71,7 +77,7 @@ const actions = [
     },
     /* index 7 */
     theDoor = {
-        description: "The door looks old.",
+        description: "The door is closed.<br>It looks to be as old as the cabin.",
         choices: ["Try turning the handle"],
         nextScene: [12]
     },
@@ -83,7 +89,7 @@ const actions = [
     },
     /* index 9 */
     theTable = {
-        description: "the Table",
+        description: "The table seems tattered.<br>On it lays a newspaper,<br>a key, guestbook and a pen.",
         choices: [],
         nextScene: []
     },
@@ -91,19 +97,55 @@ const actions = [
     writeMessage = {
         description: "The frost feels cold against my fingers.",
         choices: ["Done"],
-        nextScene: [11]
+        nextScene: [6]
     },
     /* index 11 */
-    writtenMessage = {
-        description: "The window says: ",
+    turningHandleNoKey = {
+        description: "The door seems to be locked.<br>Perhaps there is a key somewhere.",
         choices: [],
         nextScene: []
     },
     /* index 12 */
-    turningHandleNoKey = {
-        description: "The door seems to be locked.</br>Perhaps there is ",
+    turningHandleRightKey = {
+        description: "You try the silver key.<br>It fits in the lock.<br>The door is now open.",
+        choices: [],
+        nextScene: [/* Add the final scene */]
+    },
+    /* index 13 */
+    turningHandleWrongKey = {
+        description: "You try the bronze key.<br>It doesn't fit in the lock.",
         choices: [],
         nextScene: []
+    },
+    /* index 14 */
+    theNewspaper = {
+        description: "The paper has the date October 17, 1962.<br>The New York Yankees defeated the<br>San Fransisco Giants in the world series.",
+        choices: [],
+        nextScene: []
+    },
+    /* index 15 */
+    theKey = {
+        description: "A bronze key.",
+        choices: ["Pick up the key"],
+        nextScene: [15]
+    },
+    /* index 16 */
+    theGuestbook = {
+        description: "The newest entry is from 1962.<br>It was signed by Walter Buck.",
+        choices: ["Sign the guestbook"],
+        nextScene: [18]
+    },
+    /* index 17 */
+    thePen = {
+        description: "A regular pen.",
+        choices: ["Pick up the Pen"],
+        nextScene: [17]
+    },
+    /* index 18 */
+    signTheGuestbook = {
+        description: "Should I really sign my real name,<br> I still don't know how I got here...",
+        choices: ["Done"],
+        nextScene: [16]
     }
 ];
 
@@ -135,18 +177,24 @@ function changeButtons() {
     }
 }
 
-subButton.addEventListener("click", getMessage);
+/*subButton.addEventListener("click", getMessage);
 
 function getMessage() {
     let message = document.querySelector("#input-message").value;
-    actions[11].description += message;
+    let name = document.querySelector("#input-message").value;
     messageWrittenOnWindow = true;
+    nameWritteninGuestbook = true;
     if (messageWrittenOnWindow === true) {
         actions[6].description = "The window says: " + message;
         actions[6].choices = [];
         actions[6].nextScene = [];
     }
-}
+    if (messageWrittenOnWindow === true) {
+        actions[16].description = "I decided to write " + name + "in the guestbook.";
+        actions[16].choices = [];
+        actions[16].nextScene = [];
+    }
+}*/
 
 let btn = document.querySelector('.button-container');
     document.addEventListener('click', e => {
@@ -174,6 +222,7 @@ function changeScene(answer) {
             changeDescription();
             changeButtons();
             changeSceneColors();
+            secondaryMenu.style.display = "none";
         }
         else if (answer === "The door") {
             currentScene = 7;
@@ -181,6 +230,7 @@ function changeScene(answer) {
             changeDescription();
             changeButtons();
             changeSceneColors();
+            secondaryMenu.style.display = "none";
         } 
         else if (answer === "The fireplace") {
             currentScene = 8;
@@ -188,6 +238,7 @@ function changeScene(answer) {
             changeDescription();
             changeButtons();
             changeSceneColors();
+            secondaryMenu.style.display = "none";
         } 
         else if (answer === "The table") {
             currentScene = 9;
@@ -195,6 +246,29 @@ function changeScene(answer) {
             changeDescription();
             changeButtons();
             changeSceneColors();
+            secondaryMenu.style.display = "flex";
+        }
+        else {
+            if (answer === "The newspaper") {
+                currentScene = 14;
+                changeDescription();
+                changeButtons();
+            }
+            else if (answer === "The key") {
+                currentScene = 15;
+                changeDescription();
+                changeButtons();
+            }
+            else if (answer === "The guestbook") {
+                currentScene = 16;
+                changeDescription();
+                changeButtons();
+            }
+            else if (answer === "The pen") {
+                currentScene = 17;
+                changeDescription();
+                changeButtons();
+            }
         }
     }
 
@@ -202,16 +276,21 @@ function changeScene(answer) {
         matchCounter--;
     }
 
-    if (currentScene > 4 ) {
-        changeSceneColors();
-    }
-
     if (currentScene === 10) {
-        inputMessage.style.display = "flex";
-        buttonWrapper.style.display = "none"
+    element.appendChild(messageInput);
+    messageInput.setAttribute("style", "display:flex; width: 80%")
+    }
+    else if (currentScene === 18) {
+    element.appendChild(nameInput);
+    nameInput.setAttribute("style", "display:flex; width: 80%")
     }
     else {
-        inputMessage.style.display = "none";
+        messageInput.style.display = "none";
+        nameInput.style.display = "none";
+    }
+
+    if (currentScene > 4 ) {
+        changeSceneColors();
     }
 
     console.log(matchCounter);
